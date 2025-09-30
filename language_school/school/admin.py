c56b9f48c621d707dab041ee93bee5620f0bae7e
Adelya-yep <ahasanova375@gmail.com>
2025-09-30 14:31:30 +0300

    commit

--- language_school/school/admin.py
+++ language_school/school/admin.py
@@ -2,33 +2,27 @@ from django.contrib import admin
 from .models import Language, Level, Teacher, Course, CourseMaterial, Enrollment
 from django.utils import timezone
 
-
 @admin.register(Language)
 class LanguageAdmin(admin.ModelAdmin):
     list_display = ['name']
 
-
 @admin.register(Level)
 class LevelAdmin(admin.ModelAdmin):
     list_display = ['name', 'description']
-
 
 @admin.register(Teacher)
 class TeacherAdmin(admin.ModelAdmin):
     list_display = ['user', 'bio']
     filter_horizontal = ['languages']
 
-
 @admin.register(Course)
 class CourseAdmin(admin.ModelAdmin):
     list_display = ['title', 'language', 'level', 'price', 'teacher', 'is_active']
     list_filter = ['language', 'level', 'is_active']
 
-
 @admin.register(CourseMaterial)
 class CourseMaterialAdmin(admin.ModelAdmin):
     list_display = ['title', 'course', 'uploaded_at']
-
 
 @admin.register(Enrollment)
 class EnrollmentAdmin(admin.ModelAdmin):
@@ -46,3 +40,24 @@ class EnrollmentAdmin(admin.ModelAdmin):
 
     approve_selected.short_description = "Подтвердить выбранные заявки"
     reject_selected.short_description = "Отклонить выбранные заявки"
+
+from django.contrib import admin
+from django.contrib.auth.models import User
+from django.contrib.auth.admin import UserAdmin
+
+class CustomUserAdmin(UserAdmin):
+    filter_horizontal = ()
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        ('Personal info', {'fields': ('first_name', 'last_name', 'email')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'password1', 'password2'),
+        }),
+    )
+
+admin.site.unregister(User)
+admin.site.register(User, CustomUserAdmin)
